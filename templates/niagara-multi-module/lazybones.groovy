85d575c6e2443cbb0fda3b44290fd05d181d166f
Robert Carnecky <robert@neopsis.com>
2021-04-27 14:44:45 +0200

    Updated environment layout for multi-version-modules compilation. Noe creation of sepparated modules for 4.7, 4.8, 4.9, 4.10, ... is possible

--- templates/niagara-multi-module/lazybones.groovy
+++ templates/niagara-multi-module/lazybones.groovy
@@ -1,5 +1,6 @@
 @Grab(group="uk.co.cacoethes", module="groovy-handlebars-engine", version="0.2")
 import uk.co.cacoethes.handlebars.HandlebarsTemplateEngine
+import org.apache.commons.io.FileUtils
 import uk.co.cacoethes.util.NameType
 
 registerDefaultEngine new HandlebarsTemplateEngine()
@@ -15,10 +16,15 @@ if (projectDir.name =~ /\-/) {
 params.projectName = transformText(params.projectClassName, from: NameType.CAMEL_CASE, to: NameType.PROPERTY)
 
 params.moduleName         = ask("Module name [" + params.projectName + "]: ", params.projectName, "moduleName")
-params.moduleDescription  = ask("Module description [Niagara module]: ", "Niagara module", "moduleDescription")
+params.moduleDescription  = ask("Module description [Niagara demo module]: ", "Niagara demo module", "moduleDescription")
 params.moduleGroup        = ask("Vendor [Neopsis]: ", "Neopsis", "moduleGroup")
 params.moduleVersion      = ask("Module Version [4.7.0]: ", "4.7.0", "moduleVersion")
 params.preferredSymbol    = ask("Preferred symbol [neo] : ", "neo", "preferredSymbol")
+params.pkg                = ask("Package [com.example] : ", "com.example", "package")
+
+String packagePath        = params.pkg.replace('.' as char, '/' as char)
+File mainSourcesRoot      = new File(projectDir, 'wb/src/main/java')
+File mainSourcesPath      = new File(mainSourcesRoot, packagePath)
 
 File rtDir = new File(projectDir, 'rt')
 File uxDir = new File(projectDir, 'ux')
@@ -32,8 +38,18 @@ processTemplates 'settings.gradle', params
 processTemplates "rt/rt.gradle", params
 processTemplates "ux/ux.gradle", params
 processTemplates "wb/wb.gradle", params
-processTemplates "wb/src/main/java/Demo.java",  params
-processTemplates "wb/src/main/java/BDemo.java", params
+
+def renameFile = { File from, String path ->
+    if (from.file) {
+        File to = new File(path)
+        to.parentFile.mkdirs()
+        FileUtils.moveFile(from, to)
+    }
+}
+
+mainSourcesRoot.eachFile { File file ->
+    renameFile(file, mainSourcesPath.absolutePath + '/' + file.name)
+}
 
 rtBuild.renameTo(projectDir.name + "/rt/" + params.moduleName + "-rt.gradle")
 uxBuild.renameTo(projectDir.name + "/ux/" + params.moduleName + "-ux.gradle")
